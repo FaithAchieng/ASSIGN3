2c2fc7b8e06184b614885ee3457c1894e3821a72
FaithAchieng <135111683+FaithAchieng@users.noreply.github.com>
2023-09-20 13:44:11 +0300

    Add files via upload

---
+++ marksheet.java
@@ -0,0 +1,48 @@
+package marksheet1;
+import java.util.Scanner;
+
+public class marksheet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Student Mark List Application");
+int a,c,d,e,f,g,h,i,k,l,m,n,o,p,q,r,s,t,u,v,w,x;
+String b,j,r1;
+Scanner in=new Scanner(System.in);
+a=in.nextInt(); b=in.next(); c=in.nextInt();d=in.nextInt();e=in.nextInt();f=in.nextInt();g=in.nextInt();
+i=in.nextInt();j=in.next();k=in.nextInt();l=in.nextInt();m=in.nextInt();n=in.nextInt();o=in.nextInt();
+q=in.nextInt();r1=in.next();s=in.nextInt();t=in.nextInt();u=in.nextInt();v=in.nextInt();w=in.nextInt();
+System.out.println("Welcome to students Mark List Application");
+System.out.println("__________________________________________");
+System.out.println("SNo	Student Name	Sub1	Sub2	    Sub3	        Sub4	      Sub5	   Total");
+System.out.print(""+a);
+System.out.print("\t"+b);
+System.out.print("\t\t"+c);
+System.out.print("\t\t"+d);
+System.out.print("\t\t"+e);
+System.out.print("\t\t"+f);
+System.out.print("\t\t"+g);
+h=c+d+e+f+g;
+System.out.println("\t\t"+h);
+System.out.print(""+i);
+System.out.print("\t"+j);
+System.out.print("\t\t"+k);
+System.out.print("\t\t"+l);
+System.out.print("\t\t"+m);
+System.out.print("\t\t"+n);
+System.out.print("\t\t"+o);
+p=k+m+o+l+n;
+System.out.println("\t\t"+p);
+System.out.print(""+q);
+System.out.print("\t"+r1);
+System.out.print("\t\t"+s);
+System.out.print("\t\t"+t);
+System.out.print("\t\t"+u);
+System.out.print("\t\t"+v);
+System.out.print("\t\t"+w);
+x=s+t+u+v+w;
+System.out.print("\t\t"+x);
+
+	}
+
+}
